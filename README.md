e53b88fa547be1079525a07259b5525a12bf4a63
Andres Michel <andresmichelg@gmail.com>
2019-05-08 12:23:16 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# react-native-layout-constants
+
+## Getting Started
+
+- [Installation](#installation)
+
+### Installation
+
+```bash
+$ npm install react-native-layout-constants
+```
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { Dimensions, Platform } from 'react-native'
+import { getStatusBarHeight } from 'react-native-status-bar-height'
+
+const width = Dimensions.get('window').width
+const height = Dimensions.get('window').height
+const statusBarHeight = getStatusBarHeight(true)
+
+export default {
+  window: {
+    width,
+    height,
+  },
+  isSmallDevice: width < 375,
+  statusBarHeight: Platform.OS === 'ios' ? statusBarHeight : 0,
+  isPad: Platform.OS === 'ios' ? Platform.isPad : width > 500,
+}
